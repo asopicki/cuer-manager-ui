3919fcaac3c4249b2eaebd1c04dffc2c4fa3e2b1
Alexander Sopicki <asopicki@gmail.com>
2019-10-14 14:21:55 +0200

    player draft

--- src/app/cuecard/cuecard.component.ts
+++ src/app/cuecard/cuecard.component.ts
@@ -12,6 +12,13 @@ export class CuecardComponent implements OnInit {
 
   content: String;
 
+  audioContext;
+  audioBuffer;
+  audioSource;
+  playbackRate = 1.0;
+  playDisabled = true;
+  stopDisabled = true;
+  
   constructor(private route: ActivatedRoute, private service: CuecardService) { }
 
   ngOnInit() {
@@ -21,8 +28,69 @@ export class CuecardComponent implements OnInit {
       if (uuid) {
         this.service.getCuecard(uuid).subscribe(content => {
           document.getElementById('cuecard').innerHTML = content.toString();
+
+          let node = document.querySelector("#cuecard meta[name='x:audio-file']");
+          
+
+          let fileName = node.getAttribute('content');
+          this.service.getAudioFile(fileName).subscribe(file => this.load(file));
         });
       }
     });
+  
+  }
+
+  load(audioFile: Blob) {
+    let self = this;
+    let reader = new FileReader();
+    reader.onload = (event: any) => {
+      console.debug('File load complete')
+      let arrayBufffer = event.target.result;
+      let audioContext: any = (<any>window).AudioContext || (<any>window).webkitAudioContext;
+      let ctx = new audioContext();
+
+      let bufferSource = ctx.createBufferSource();
+      
+
+      ctx.decodeAudioData(arrayBufffer).then((audioBuffer) => {
+        console.debug('Decoded');
+        //console.log(audioBuffer);
+        bufferSource.buffer = audioBuffer;
+        //console.log(bufferSource);
+        bufferSource.connect(ctx.destination);
+        self.audioContext = ctx;
+        self.audioSource = bufferSource;
+        self.audioBuffer = audioBuffer;
+        self.playDisabled = false;
+        //console.log(ctx);
+      });
+    };
+    reader.readAsArrayBuffer(audioFile);
+  }
+
+  play() {
+    if (this.audioSource) {
+      this.audioSource.start();
+      this.playDisabled = true;
+      this.stopDisabled = false;
+    }
+  }
+
+  stop() {
+    if (this.audioSource) {
+      this.audioSource.stop();
+
+      this.audioSource = this.audioContext.createBufferSource();
+      this.audioSource.buffer = this.audioBuffer;
+      this.audioSource.connect(this.audioContext.destination);
+      this.playDisabled = false;
+      this.stopDisabled = true;
+    }
+  }
+
+  rateChange(e: any) {
+    console.log(e);
+    console.log(this.playbackRate);
+    this.audioSource.playbackRate.value = this.playbackRate;
   }
 }
